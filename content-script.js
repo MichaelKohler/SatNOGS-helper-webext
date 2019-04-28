23d669a6b9163f7be515b2eb946030c805df4043
Michael Kohler <me@michaelkohler.info>
2019-04-28 18:14:11 +0200

    Check for confirmation before closing

--- content-script.js
+++ content-script.js
@@ -3,6 +3,7 @@
 console.log('SatNOGS: Content Script loaded');
 
 const KEYS = ['g', 'b', 'f']; // TODO: verify?
+const ALERT_CHECK_INTERVAL_MS = 25;
 const DEFAULT_WAIT_TIME_MS = 500;
 let waitTime = DEFAULT_WAIT_TIME_MS;
 
@@ -21,15 +22,24 @@ document.addEventListener('keypress', (e) => {
       waitTime = DEFAULT_WAIT_TIME_MS;
     })
     .then(() => {
-      // TODO: wait for confirmation
-      return Promise.resolve();
-    })
-    .then(() => {
-      console.log(`SatNOGS: got ${waitTime}ms wait time from settings`);
-      setTimeout(() => {
-        console.log('SatNOGS: wait time over, continuing..');
-        browser.runtime.sendMessage("SatNOGS_DONE")
-          .catch((error) => console.error('SatNOGS error:', error));
-      }, waitTime);
+      console.log('SatNOGS: waiting for notification..');
+      let checkerInterval = setInterval(() => {
+        const alert = document.querySelector('#alert-messages .alert');
+        console.log('checking alert..', alert);
+        if (alert) {
+          console.log('SatNOGS: got notification, continuing..');
+          clearInterval(checkerInterval);
+          close();
+        }
+      }, ALERT_CHECK_INTERVAL_MS);
     });
 });
+
+function close() {
+  console.log(`SatNOGS: got ${waitTime}ms wait time from settings`);
+  setTimeout(() => {
+    console.log('SatNOGS: wait time over, continuing..');
+    browser.runtime.sendMessage("SatNOGS_DONE")
+      .catch((error) => console.error('SatNOGS error:', error));
+  }, waitTime);
+}
